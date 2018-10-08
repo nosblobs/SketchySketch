e1f3123a8e1c806234d2e1af12d3d948a0493293
nosblobs <nicola.k.osborne@gmail.com>
2018-10-08 23:28:23 +0100

    test

--- SketchySketch/Controllers/ValuesController.cs
+++ SketchySketch/Controllers/ValuesController.cs
@@ -11,7 +11,7 @@ namespace SketchySketch.Controllers {
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get() {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "ozzy smells" };
         }
 
         // GET api/values/5
